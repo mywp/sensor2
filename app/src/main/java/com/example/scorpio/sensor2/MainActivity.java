5b9087f870efdcdb8b4f670995de7b1cf6686056
Scorpio <Scorpio@MacBook-Pro.workgroup>
2016-04-11 18:43:09 +0800

    Initial commit

---
+++ app/src/main/java/com/example/scorpio/sensor2/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.scorpio.sensor2;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SensorManager sm;
+    private ImageView iv;
+    private MyListener listener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+        iv = (ImageView) findViewById(R.id.iv);
+        //方向传感器
+        Sensor sensor = sm.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+        listener = new MyListener();
+        sm.registerListener(listener, sensor, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    private class MyListener implements SensorEventListener {
+
+        float lastangle =0;
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            //0=North ,90=East,180=South ,270 =West
+            float angle =event.values[0];//手机与正北方向的夹角
+            System.out.println("angle；"+angle);
+            RotateAnimation ra = new RotateAnimation(-lastangle, angle, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+            iv.startAnimation(ra);
+            lastangle=angle;
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        sm.unregisterListener(listener);
+        super.onDestroy();
+    }
+}
